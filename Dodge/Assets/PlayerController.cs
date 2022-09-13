cae8e88ed3a46eb8f4d3500657774f8697ecd64c
김우남 <gimunam02@gmail.com>
2022-09-13 17:35:45 +0900

    20220913
    파랑색 오브젝트

---
+++ Dodge/Assets/PlayerController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // 이동에 사용할 리지드바디 컴퍼넌트
+    public Rigidbody playerRigidbody;
+    public float speed = 8f; // 이동 속력
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
