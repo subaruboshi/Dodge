1f45746c3baa0ad8ad5e2695ee2e8c6bd61599c5
김우남 <gimunam02@gmail.com>
2022-09-21 12:41:18 +0900

    20220921
    로테이션

---
+++ Dodge/Assets/02.Scripts/Rotator.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    public float rotationSpeed = 60f;
+
+    private void Update()
+    {
+        transform.Rotate(0f, rotationSpeed, 0f);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
